3db93cb8bf7a6df6d0477d3da34fd11ffd119378
43209253 <43209253+Fardeenbholai@users.noreply.github.com>
2020-11-27 16:52:48 +0100

    ec2 instance with webserver!

--- testing.tf
+++ testing.tf
@@ -1,5 +1,12 @@
 resource "aws_instance" "first_instance" {
-  ami = "ami-04bf6dcdc9ab498ca"
+  ami = "ami-04d29b6f966df1537"
   instance_type = "t2.micro"
+  user_data = "${file("userdata.sh")}"
 }
-#hallo
+
+output "instanceip" {
+  value = aws_instance.first_instance.public_ip
+
+
+
+}
