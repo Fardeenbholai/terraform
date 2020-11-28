d11632a903970be6a57e9b591f50577ce0077238
JakeHU1 <43312270+JakeHU1@users.noreply.github.com>
2020-11-28 11:47:51 +0100

    create public and private subnets

--- variables.tf
+++ variables.tf
@@ -1,13 +1,18 @@
 variable "vpc_cidr" {
-	default = "10.0.0.0/16"
-} 
+  default = "10.0.0.0/16"
+}
 
-variable "subnets_cidr" {
-	type = "list"
-	default = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "subnets_cidr_private" {
+  type    = "list"
+  default = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "subnets_cidr_public" {
+  type    = "list"
+  default = ["10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "azs" {
-	type = "list"
-	default = ["us-east-1a", "us-east-1b"]
+  type    = "list"
+  default = ["us-east-1a", "us-east-1b"]
 } 
