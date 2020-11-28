ffc2106d5140a908f9630262fb827d2b20973865
JakeHU1 <43312270+JakeHU1@users.noreply.github.com>
2020-11-28 11:47:51 +0100

    create public and private subnets

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "terraform_vpc" {
-  cidr_block       = "${var.vpc_cidr}"
+  cidr_block = "${var.vpc_cidr}"
   tags = {
     Name = "terraformvpc"
   }
@@ -8,7 +8,29 @@ resource "aws_vpc" "terraform_vpc" {
 # creates an internet gateway inside terraformvpc
 resource "aws_internet_gateway" "terra_igw" {
   vpc_id = "${aws_vpc.terraform_vpc.id}"
-  tags = { 
+  tags = {
     Name = "main"
+  }
+}
+
+# subnets: public
+resource "aws_subnet" "public" {
+  count             = "${length(var.subnets_cidr_public)}"
+  vpc_id            = "${aws_vpc.terraform_vpc.id}"
+  cidr_block        = "${element(var.subnets_cidr_public, count.index)}"
+  availability_zone = "${element(var.azs, count.index)}"
+  tags = {
+    "Name" = "Public-${count.index + 1}"
+  }
+}
+
+# subnets : private
+resource "aws_subnet" "private" {
+  count             = "${length(var.subnets_cidr_private)}"
+  vpc_id            = "${aws_vpc.terraform_vpc.id}"
+  cidr_block        = "${element(var.subnets_cidr_private, count.index)}"
+  availability_zone = "${element(var.azs, count.index)}"
+  tags = {
+    "Name" = "Private-${count.index + 1}"
   }
 }
