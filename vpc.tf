6e434f47d39a7bc4454fcd152c2107dfd4b30ccd
JakeHU1 <43312270+JakeHU1@users.noreply.github.com>
2020-11-28 12:31:19 +0100

    added nat gateway

--- vpc.tf
+++ vpc.tf
@@ -34,3 +34,40 @@ resource "aws_subnet" "private" {
     "Name" = "Private-${count.index + 1}"
   }
 }
+
+# eip for NAT gateway
+resource "aws_eip" "nat" {
+  count = "${length(var.subnets_cidr_public)}"
+  vpc   = true
+
+  # new replacement object is created first, and then the prior object is destroyed only once the replacement is created.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# NAT gateway
+resource "aws_nat_gateway" "nat" {
+  count         = "${length(var.subnets_cidr_public)}"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  subnet_id     = "${element("${aws_subnet.public.*.id}", count.index)}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    "Name" = "NAT-gateway public ${count.index + 1}"
+  }
+  depends_on = [aws_subnet.public, ]
+}
+
+resource "aws_route_table" "nat" {
+  count  = "${length(var.subnets_cidr_public)}"
+  vpc_id = "${aws_vpc.terraform_vpc.id}"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${element(aws_nat_gateway.nat.*.id, count.index)}"
+  }
+}
