0b3aa46223cb0464002894d1b90c812cd6305d9c
JakeHU1 <43312270+JakeHU1@users.noreply.github.com>
2020-11-28 16:22:52 +0100

    packer for AMI building

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Install Apache Web Server and PHP
-yum install -y httpd mysql php
+sudo yum install -y httpd mysql php
 # Download Lab files
-wget https://aws-tc-largeobjects.s3.amazonaws.com/AWS-TC-AcademyACF/acf-lab3-vpc/lab-app.zip
-unzip lab-app.zip -d /var/www/html/
+sudo wget https://aws-tc-largeobjects.s3.amazonaws.com/AWS-TC-AcademyACF/acf-lab3-vpc/lab-app.zip
+sudo unzip lab-app.zip -d /var/www/html/
 # Turn on web server
-chkconfig httpd on
-service httpd start
+sudo systemctl start httpd
+sudo systemctl enable httpd
