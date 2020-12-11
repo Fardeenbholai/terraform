cf4c3895a619561af0e546f7d93605a64ceaea63
Jake <jake1998@hotmail.nl>
2020-12-11 21:28:41 +0100

    Terraform and Packer integration

---
+++ rollout.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# run Packer and pipe and transport output to readable file for Terraform
+# source: https://discuss.devopscube.com/t/how-to-get-the-ami-id-after-a-packer-build/36
+packer build -machine-readable ami.json | grep 'artifact,0,id' | cut -d, -f6 | cut -d: -f2 > ami.txt
+
+# run Terraform to provision the resources
+terraform apply
